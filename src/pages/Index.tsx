0a37d87da9fefeff13b6f95702a3b9530053f675
beetwenty <sindrebergee96@gmail.com>
2025-03-05 21:39:48 +0100

    removed double routines

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -274,7 +274,7 @@ const Index = () => {
           <ExerciseHistoryTracker />
         </TabsContent>
       </Tabs>
-
+{/* 
       <section className="py-4">
         <h2 className="text-2xl font-semibold mb-4 dark:text-white">Routines</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -301,7 +301,7 @@ const Index = () => {
           )}
         </div>
       </section>
-
+*/}
       <section className="py-4">
         <WeightTracker />
       </section>
